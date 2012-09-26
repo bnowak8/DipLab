4e7aece9a1bb94198f53de559a812324ba97749e
mashit <mashit@66-152-109-110.tvc-ip.com>
2012-09-26 08:35:44 -0500

    Compared to instructor solution,  Inserted missing data validation.

--- src/dip/lab1/student/solution1/HourlyEmployee.java
+++ src/dip/lab1/student/solution1/HourlyEmployee.java
@@ -15,9 +15,34 @@ public class HourlyEmployee implements Employee {
     private double annualBonus;
     
     public HourlyEmployee(double hourlyRate, double totalHrsForYear, double annualBonus){
+        
         this.hourlyRate = hourlyRate;
         this.totalHrsForYear = totalHrsForYear;
         this.annualBonus = annualBonus;
+    }
+
+    public double getHourlyRate() {
+        return hourlyRate;
+    }
+
+    public void setHourlyRate(double hourlyRate) {
+        
+        if(hourlyRate < 0 || hourlyRate > 500) {
+            throw new IllegalArgumentException();
+        }
+        this.hourlyRate = hourlyRate;
+ 
+    }
+
+    public double getTotalHrsForYear() {
+        return totalHrsForYear;
+    }
+
+    public void setTotalHrsForYear(double totalHrsForYear) {
+        if(totalHrsForYear < 0 || totalHrsForYear > 5000) {
+            throw new IllegalArgumentException();
+        }
+        this.totalHrsForYear = totalHrsForYear;
     }
 
 
